a838840cc3b451f4ef9f934141c79614488ecf9f
thierry dothee <thierrydothee@protonmail.com>
2023-06-07 08:18:57 +0200

    correction bug methode ajoutContact

--- src/fr/thierry/phonebook/Contact.java
+++ src/fr/thierry/phonebook/Contact.java
@@ -1,65 +1,70 @@
 package fr.thierry.phonebook;
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.List;
 
-public class Contact extends PhoneDirectory {
+public class Contact {
 
-	public Contact(String nom, String numero) {
-		super();
-		this.nom = nom;
-		this.numero = numero;
-	}
+	private static String nom;
+	private static String numero;
+	static Scanner scanner = new Scanner(System.in);
 	
-	public Contact() {
-		// TODO Auto-generated constructor stub
-	}
 
-	private String nom;
-	private String numero;
-	protected ArrayList<Contact> list = new ArrayList<Contact>();
-	static Scanner scan = new Scanner(System.in);
-	
 	public String getNom() {
 		return nom;
 	}
 
-
+	@SuppressWarnings("static-access")
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
 
 	public String getNumero() {
 		return numero;
 	}
 
-
+	@SuppressWarnings("static-access")
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-	
-	
-	public  void ajouterContact(Contact contact) {	
-			list.add(contact);
-	}
-	
-	public static void main(String[] args) {
-		
-		System.out.println("Ajouter un nom");
-		String name = scan.nextLine();
-		System.out.println("Ajouter un numero");
-		String number = scan.nextLine();
-		Contact contact = new Contact(name,number);
-		contact.ajouterContact(contact);
-		System.out.println(contact.getNom()+ " " +contact.getNumero());
-		
-		scan.close();
-		
-		
+
+	public Contact() {
+		super();
 	}
 
+	@SuppressWarnings("static-access")
+	public Contact(String nom, String numero) {
+		super();
+		this.nom = nom;
+		this.numero = numero;
+	}
 
+	public Contact ajouterContact(String nom, String nombre) {
+		Contact contact = new Contact(nom, nombre);
+		return contact;
+	}
 
+	public void nombreContact(List<Contact> list) {
+		System.out.println("Indiquez le nombre de contact :");
+		int count = scanner.nextInt();
+		String[] names = new String[count];
+		String[] numbers = new String[count];
+		scanner.nextLine();
+		int j = 0;
+		for (int i = 0; i < count; i++) {
+			System.out.println("Contact n°" + ++j);
+			System.out.print("Saisir son nom : ");
+			String name = scanner.nextLine();
+			names[i] = name;
+			System.out.print("Saisir son numéro : ");
+			String number = scanner.nextLine();
+			numbers[i] = number;
+			System.out.println("----------------\n");
 
+		}
+		for (int k = 0; k < names.length; k++) {
+			list.add(ajouterContact(names[k], numbers[k]));
+			System.out.println(list.get(k).getNom() + " " + list.get(k).getNumero());
+		}
+	}
 }
--- src/fr/thierry/phonebook/PhoneDirectory.java
+++ src/fr/thierry/phonebook/PhoneDirectory.java
@@ -1,16 +1,35 @@
 package fr.thierry.phonebook;
 
-
-
-public class PhoneDirectory  {
+import java.util.ArrayList;
+public class PhoneDirectory extends Contact {
 	
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
+	public PhoneDirectory(ArrayList<Contact> list) {
+		super();
+		PhoneDirectory.list = list;
 	}
+
+	protected static ArrayList<Contact>list = new ArrayList<Contact>();
+
+	public ArrayList<Contact> getList() {
+		return list;
+	}
+
+	public void setList(ArrayList<Contact> list) {
+		PhoneDirectory.list = list;
+	}
+	
+	
+	public static void main(String[] args) {
+		Contact contact = new Contact();
+		contact.nombreContact(list);
+		scanner.close();
+
+	}
+
+
+
+
+
 	
 
 
