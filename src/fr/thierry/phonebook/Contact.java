6443afb4b4372e18a47ee5eaaeb5765459d113e9
thierry dothee <thierrydothee@protonmail.com>
2023-06-08 17:38:29 +0200

    ajout methodes : ajouter,supprimer,rechercher

--- src/fr/thierry/phonebook/Contact.java
+++ src/fr/thierry/phonebook/Contact.java
@@ -1,3 +1,4 @@
+
 package fr.thierry.phonebook;
 
 import java.util.Scanner;
@@ -6,85 +7,133 @@ import java.util.List;
 public class Contact {
 	
 	// attributs
-	private static String nom;
-	private static String numero;
-	static Scanner scanner = new Scanner(System.in);
+	private String nom;
+	private String numero;
+	private static Scanner scanner = new Scanner(System.in);
 	
 	// getter nom
 	public String getNom() {
 		return nom;
 	}
+	
 	// setter nom
-	@SuppressWarnings("static-access")
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
 	// getter numero
 	public String getNumero() {
 		return numero;
 	}
-	//setter numero
-	@SuppressWarnings("static-access")
+	
+	// setter numero
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+	
 	// constructor surcharge
 	public Contact() {
 		super();
 	}
+	
 	//constructor
-	@SuppressWarnings("static-access")
 	public Contact(String nom, String numero) {
 		this.nom = nom;
 		this.numero = numero;
 	}
 
-	// methode pour initialise un objet Contact
-	public Contact ajouterContact(String nom, String nombre) {
-		Contact contact = new Contact(nom, nombre);
+	// methode pour initialiser un objet Contact
+	public static Contact ajouterContact(String nom, String numero) {
+		Contact contact = new Contact(nom, numero);
 		return contact;
+	}
+	
+	public void showData(Contact contact) {
+		System.out.println("Nom du contact = " + contact.getNom() + ", Telephone = " + contact.getNumero());
+		System.out.println();
 	}
 	
 	// methode ajouter des contacts
 	public void nombreContact(List<Contact> list) {
-		System.out.println("Indiquez le nombre de contact :");
+		System.out.println("Indiquez le nombre de contacts :");
 		// scanner permet de recuperer la saisie au clavier
 		int count = scanner.nextInt();
 		// controle savoir si count est un entier (int)
-		if(count ==(int)count) {
+		if(count == (int) count) {
 			try {
-				// creation tableaux de noms et de nombres
-				String[] names = new String[count];
-				String[] numbers = new String[count];
-				scanner.nextLine();
-				int j = 0;
-				for (int i = 0; i < count; i++) { // boucle pour la saisie des noms et des nombres ( names et numbers)
-					System.out.println("Contact n°" + ++j);   // ligne "contact n°1"
+				scanner.nextLine(); // vider le tampon de la ligne précédente
+				for (int i = 0; i < count; i++) { // boucle pour la saisie des noms et des nombres (names et numbers)
+					System.out.println("Contact n°" + (i + 1)); // ligne "contact n°1"
 					System.out.print("Saisir son nom : ");
-					String name = scanner.nextLine().toLowerCase();  // tolowerCase trasforme toutes les lettres en minuscule
-					if (!name.isEmpty() && name.matches("^[a-z]*$") )  // controle pas vide et conforme aux lettres de l'alphabet
-					names[i] = name;
-					else {
-						System.out.println("Votre saisie n'est pas une chaine de caractères");
-					}
-					System.out.print("Saisir son numéro : ");
-					String number = scanner.nextLine().toLowerCase();
-					if(!number.isEmpty() && number.matches("^[0-9]{10}*$")) { //controle si 10 chiffres telephone
-					numbers[i] = number;}
-					else {
-						System.out.println("Votre saisie n'est pas une chaine de caractères");
+					String name = scanner.nextLine().toLowerCase(); // toLowerCase transforme toutes les lettres en minuscule
+					if (!name.isEmpty() && name.matches("^[a-zA-Z]*$")) { // controle pas vide et conforme aux lettres de l'alphabet
+						System.out.print("Saisir son numéro : ");
+						String number = scanner.nextLine();
+						if (!number.isEmpty() && number.matches("^[0-9]*$")) { // controle si 10 chiffres telephone
+							list.add(ajouterContact(name, number)); // <---- liste d'objets Contact ajout de contact.nom - contact.numero
+						} else {
+							System.out.println("Votre saisie n'est pas un numéro valide");
+							i--;
+						}
+					} else {
+						System.out.println("Votre saisie n'est pas une chaîne de caractères");
+						i--;
 					}
 					System.out.println("----------------\n");
 
 				}
-				for (int k = 0; k < names.length; k++) {  // boucle for / taille du tableau names (names.length)
-					list.add(ajouterContact(names[k], numbers[k]));  // <---- list d'objet Contact ajout de contact.nom - contact.numero
-					System.out.println(list.get(k).getNom() + " " + list.get(k).getNumero());
-				}
 			} catch (Exception e) {
-				System.out.println("Votre saisie n'est pas une valeur numériqe");
+				System.out.println("Votre saisie n'est pas un nombre valide");
 			}
-
 		}
 	}
+	
+	public void supprimerContact(List<Contact> list) {
+		 boolean contactASupprimer = false;
+			System.out.println("Indiquez le nom de contact a supprimer: ");
+			String nom= scanner.nextLine();
+		
+			if(!nom.isEmpty() && nom.matches("^[A-Za-z ]*$")) {
+			// Parcourir la liste des contacts
+			 for (int i=0; i<list.size();i++) {
+				if (list.get(i).getNom().equalsIgnoreCase(nom)) {
+					list.remove(i);
+					contactASupprimer = true;
+					break;
+				}
+				}
+		}
+			
+			if (contactASupprimer != false) {
+				
+				System.out.println("Le contact '" + nom + "' a ete supprime.");
+			} else {
+				System.out.println("Le contact '" + nom + "' n'existe pas dans la liste.");
+			}
+		}
+		public void rechercherContact(List<Contact>list) {
+			boolean contactRechercher = false;
+			String indexContact ="";
+			System.out.println("Indiquez le nom de contact à rechercher");
+			String nom = scanner.nextLine();
+			
+			if(!nom.isEmpty() && nom.matches("^[A-Za-z ]*$")) {
+				//parcourir la liste des contacts
+				for (int i=0; i<list.size();i++) {
+					if (list.get(i).getNom().equalsIgnoreCase(nom)) {
+						contactRechercher= true;
+						indexContact=list.get(i).getNumero().toString();
+						break;									
+					}
+				}
+			}
+			if (contactRechercher != false) {
+				
+				System.out.println("Le contact '" + nom + "' existe dans la liste. à l'index " + indexContact);
+			} else {
+				System.out.println("Le contact '" + nom + "' n'existe pas dans la liste.");
+			}
+		}
+	
+
 }
--- src/fr/thierry/phonebook/PhoneDirectory.java
+++ src/fr/thierry/phonebook/PhoneDirectory.java
@@ -1,41 +1,45 @@
 package fr.thierry.phonebook;
 
-import java.util.ArrayList;
-public class PhoneDirectory extends Contact {
-	// constructor  
-	public PhoneDirectory(ArrayList<Contact> list) {
-		super();
-		PhoneDirectory.list = list;
-	}
 
-	protected static ArrayList<Contact>list = new ArrayList<Contact>();
-	// getter
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PhoneDirectory {
+	
+	private static ArrayList<Contact> list = new ArrayList<>(); // Liste des contacts du répertoire téléphonique
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Contact contact = new Contact(); // Création d'un objet Contact pour gérer les contacts
+		contact.nombreContact(list); // Appel de la méthode pour ajouter des contacts dans la liste
+		
+		PhoneDirectory phoneDirectory = new PhoneDirectory(list); // Création d'un objet PhoneDirectory pour gérer le répertoire téléphonique
+		phoneDirectory.afficherContacts(); // Appel de la méthode pour afficher les contacts du répertoire
+		contact.supprimerContact(list);
+		phoneDirectory.afficherContacts();
+		contact.rechercherContact(list);
+		phoneDirectory.afficherContacts();
+		scanner.close(); // Fermeture du scanner
+	}
+	
+	
+	public PhoneDirectory(ArrayList<Contact> list) {
+		this.list = list;
+	}
+	
 	public ArrayList<Contact> getList() {
 		return list;
 	}
-	// setter
+	
+	
 	public void setList(ArrayList<Contact> list) {
-		PhoneDirectory.list = list;
-	}
-	// constructor surcharge
-	public PhoneDirectory() {
-		super();
+		this.list = list;
 	}
 	
-	
-	public static void main(String[] args) {
-		Contact contact = new Contact();  // creation d'un objet Contact
-		contact.nombreContact(list);  // appel de la methode nombreContact qui est dans la classe Contact
-		scanner.close();
-
+	public void afficherContacts() {
+		for (Contact contact : list) {
+			System.out.println("Nom : " + contact.getNom() + ", Numéro : " + contact.getNumero());
+		}
 	}
-
-
-
-
-
-
-	
-
-
 }
